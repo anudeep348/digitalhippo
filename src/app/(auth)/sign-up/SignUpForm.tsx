536e48d5c6d9be1f64aac8f22e64b8af65c7557d
Anudeep Chowdary <sirigirianudeep348@gmail.com>
2024-07-16 06:56:20 +0530

    Addedd toast notifaction, handled error and success state in sign-up From

--- src/app/(auth)/sign-up/SignUpForm.tsx
+++ src/app/(auth)/sign-up/SignUpForm.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useToast } from "@/components/ui/use-toast";
 import { cn } from "@/lib/utils";
 import {
   AuthCredentialsValidaor,
@@ -10,9 +11,13 @@ import {
 } from "@/lib/validators/accountCredentialValidators";
 import { trpc } from "@/trpc/client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { ZodError } from "zod";
 
 const SignUpForm = () => {
+  const { toast } = useToast();
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -21,7 +26,39 @@ const SignUpForm = () => {
     resolver: zodResolver(AuthCredentialsValidaor),
   });
 
-  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
+    onError: (err) => {
+      if (err.data?.code === "CONFLICT") {
+        toast({
+          description: "This email is already in use. Sign in instead?",
+          variant: "destructive",
+        });
+
+        return;
+      }
+
+      if (err instanceof ZodError) {
+        toast({
+          description: err.issues[0].message,
+          variant: "destructive",
+        });
+
+        return;
+      }
+
+      toast({
+        description: "something went wrong.Please try again after some time",
+        variant: "destructive",
+      });
+    },
+    onSuccess: ({ sentToEmail }) => {
+      toast({
+        description: `Verification email sent to ${sentToEmail}`,
+        variant: "success",
+      });
+      router.push("/verify-email?to=" + sentToEmail);
+    },
+  });
 
   const onsubmit = ({ email, password }: TAuthCredentialsValidaor) => {
     mutate({ email, password });
@@ -40,6 +77,9 @@ const SignUpForm = () => {
             })}
             placeholder="you@example.com"
           />
+          {errors?.email && (
+            <p className="text-sm text-red-500">{errors?.email?.message}</p>
+          )}
         </div>
 
         <div className="grid gap-1 py-2">
@@ -53,9 +93,12 @@ const SignUpForm = () => {
             })}
             placeholder="password"
           />
+          {errors?.password && (
+            <p className="text-sm text-red-500">{errors?.password?.message}</p>
+          )}
         </div>
 
-        <Button>Sign up</Button>
+        <Button disabled={isLoading}>Sign up</Button>
       </div>
     </form>
   );
